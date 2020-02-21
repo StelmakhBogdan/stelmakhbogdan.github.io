13dbb77645a2a8365601bd6321add23e88571f0a
Bogdan300595 <61137351+Bogdan300595@users.noreply.github.com>
2020-02-21 13:45:30 +0530

    Male model site

---
+++ js/script.js
@@ -0,0 +1,55 @@
+/* Navigation Menu */
+var menuToggle = document.getElementById('menu-toggle')
+var mobileNavContainer = document.getElementById('mobile-nav')
+menuToggle.onclick = function myFunction() {
+  menuToggle.classList.toggle('menu-icon-active');
+  mobileNavContainer.classList.toggle('mobile-nav-active');
+}
+
+
+/* Slider */
+var slideIndex = 1;
+showSlides(slideIndex);
+
+/* Функция увеличивает индекс на 1, показывает следующй слайд*/
+function plusSlide() {
+    showSlides(slideIndex += 1);
+}
+
+/* Функция уменьшяет индекс на 1, показывает предыдущий слайд*/
+function minusSlide() {
+    showSlides(slideIndex -= 1);
+}
+
+/* Устанавливает текущий слайд */
+function currentSlide(n) {
+    showSlides(slideIndex = n);
+}
+
+/* Основная функция слайдера */
+function showSlides(n) {
+    var i;
+    var slides = document.getElementsByClassName("item");
+    var dots = document.getElementsByClassName("slider-dots_item");
+
+    if (n > slides.length) {
+      slideIndex = 1
+    }
+    if (n < 1) {
+        slideIndex = slides.length
+    }
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex - 1].style.display = "block";
+    dots[slideIndex - 1].className += " active";
+}
+
+/* Функция увеличивает индекс на 1, показывает следующй слайд через 5с*/
+var timer = setInterval(myFunction, 5000);
+function myFunction() {
+  showSlides(++slideIndex);
+}
